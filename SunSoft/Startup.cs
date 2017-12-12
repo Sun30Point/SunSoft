acd687aaf2f0b6b4bb5bd16001413f1161cab338
DELL <DELL@DELL->
2017-12-12 23:33:53 +0200

    Добавьте файлы проекта.

---
+++ SunSoft/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SunSoft.Startup))]
+namespace SunSoft
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
